dab4f2e3e7bbb1d85b08e7788eb1ed0da360184a
Matthew Dolan <matthew.dolan@ballysinteractive.com>
2022-09-30 07:37:09 -0700

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Matthew Dolan on 2022-09-30.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
